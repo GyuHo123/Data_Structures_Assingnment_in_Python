c9cbd5d0e08ddd943a8ea1d7a844ce44cb5e4c1b
GyuHo123 <74482513+GyuHo123@users.noreply.github.com>
2022-04-06 21:18:41 +0900

    Complete HW1_3.py

--- HW1_3.py
+++ HW1_3.py
@@ -16,12 +16,15 @@ class Node(object):
         return self.previous_node
     def set_previous(self, new_next):
         self.previous_node = new_next
+
 class DoublyLinkedList(MyList):
     def __init__(self, head=None):
         self.head = None
         self.length = 0
+
     def len(self):
         return self.length
+
     def getitem(self, j):
         if (self.length > j):
             current = self.head
@@ -29,6 +32,7 @@ class DoublyLinkedList(MyList):
                 current = current.next_node
             return current.data
         raise ValueError('value not in list')
+
     def setitem(self, val, j):
         if (self.length > j):
             current = self.head
@@ -37,6 +41,7 @@ class DoublyLinkedList(MyList):
             current.data = val
             return
         raise ValueError('index is out of bound')
+
     def insertItem(self, item, j=0):
         new_node = Node(item)
         if j == 0:
@@ -50,7 +55,7 @@ class DoublyLinkedList(MyList):
                 self.head = new_node
                 self.length += 1
                 return
-        elif j < self.length:
+        elif j < self.length - 1:
             current = self.head
             for i in range(j - 1):
                 current = current.next_node
@@ -60,34 +65,45 @@ class DoublyLinkedList(MyList):
             new_node.set_next(temp)
             temp.set_previous(new_node)
             self.length += 1
-        elif j == self.length:
+        elif j == self.length-1:
             current = self.head
             for i in range(j - 1):
                 current = current.next_node
             current.next_node = new_node
             new_node.previous_node = current
             self.length += 1
+
     def removeItem(self, j=0):
         if j == 0:
+            if self.length == 1:
+                new_node = self.head.previous_node
+                new_node = None
+                self.head = new_node
+                print("No data")
+                self.length -= 1
+            else:
                 self.head = self.head.next_node
                 self.head.previous_node = None
                 self.length -= 1
-        elif j < self.length:
+
+        elif j > 0 and j < self.length-1:
             current = self.head
             for i in range(j):
                 current = current.next_node
             current.previous_node.set_next(current.next_node)
             current.next_node.set_previous(current.previous_node)
             self.length -= 1
-        elif j == self.length:
+
+        elif j == self.length-1:
             current = self.head
             for i in range(j):
                 current = current.next_node
-            current.previous_node.next_node
-            current.previous.
-        elif j > self.length:
-            raise ValueError('index out of bound')
+            current.previous_node.next_node = None
             self.length -= 1
+
+        elif j >= self.length:
+            raise ValueError('index out of bound')
+
     def printMyList(self):
         curr = self.head
         while curr != None:
